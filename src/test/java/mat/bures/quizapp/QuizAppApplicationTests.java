e7e6e88912a71b23555024c6e7f07d8fbb4192b9
Matyáš Bureš <117597975+MatBures@users.noreply.github.com>
2023-09-08 11:22:46 +0200

    Fix GHActions

--- src/test/java/mat/bures/quizapp/QuizAppApplicationTests.java
+++ src/test/java/mat/bures/quizapp/QuizAppApplicationTests.java
@@ -2,9 +2,30 @@ package mat.bures.quizapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@DirtiesContext
 class QuizAppApplicationTests {
+    @Container
+    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest")
+            .withDatabaseName("postgres")
+            .withUsername("postgres")
+            .withPassword("admin");
+
+    @DynamicPropertySource
+    static void configurePostgres(DynamicPropertyRegistry registry) {
+        postgres.start();
+        registry.add("spring.datasource.url", () -> String.format("jdbc:postgresql://localhost:%d/postgres", postgres.getFirstMappedPort()));
+        registry.add("spring.datasource.username", () -> "postgres");
+        registry.add("spring.datasource.password", () -> "admin");
+    }
 
     @Test
     void contextLoads() {
